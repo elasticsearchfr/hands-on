8ab34e991ccf97b48d5c74b8595223c8f9805270
David Pilato <david@pilato.fr>
2012-08-03 18:23:57 +0200

    Add google tests and play with scoring

--- src/test/java/org/elasticsearchfr/handson/ex2/SearchTest.java
+++ src/test/java/org/elasticsearchfr/handson/ex2/SearchTest.java
@@ -9,12 +9,15 @@ import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.ESLoggerFactory;
+import org.elasticsearch.index.query.FilterBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearchfr.handson.StartNode;
 import org.elasticsearchfr.handson.beans.Beer;
 import org.elasticsearchfr.handson.beans.BeerHelper;
+import org.elasticsearchfr.handson.beans.Colour;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -77,6 +80,7 @@ public class SearchTest extends StartNode {
 	 * <br>We should have 1000 results
 	 * <br>We want to display the _source content of the first Hit.
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/match-all-query.html
 	 */
 	@Test
@@ -100,6 +104,7 @@ public class SearchTest extends StartNode {
 	 * We want to build a termQuery Query to find "Heineken" beers.
 	 * <br>But we won't have any result. Could you explain why?
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/term-query.html
 	 */
 	@Test
@@ -117,12 +122,15 @@ public class SearchTest extends StartNode {
 		
 		// We have 0 hit. Why?
 		Assert.assertEquals(0, sr.getHits().getTotalHits());
+
+		logger.info("We found {} beer. Why?", sr.getHits().totalHits());
 	}
 
 	/**
 	 * We want to build a termQuery Query
 	 * <br>We should have some results (or we are really unlucky!). So fix the previous test {@link #termSearch_not_working()}
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/term-query.html
 	 */
 	@Test
@@ -139,6 +147,7 @@ public class SearchTest extends StartNode {
 		Assert.assertNotNull(sr.getHits());
 		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
 
+		logger.info("We found {} beers", sr.getHits().totalHits());
 	}
 
 	/**
@@ -146,6 +155,7 @@ public class SearchTest extends StartNode {
 	 * <br>We should have some results (or we are really unlucky!).
 	 * <br>Note that we can search "HEINEKEN is a beer"
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/text-query.html
 	 */
 	@Test
@@ -161,6 +171,8 @@ public class SearchTest extends StartNode {
 		Assert.assertNotNull(sr);
 		Assert.assertNotNull(sr.getHits());
 		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
+
+		logger.info("We found {} beers", sr.getHits().totalHits());
 	}
 
 	/**
@@ -168,6 +180,7 @@ public class SearchTest extends StartNode {
 	 * <br>We should have some results (or we are really unlucky!).
 	 * <br>Note that we can search "HEINEKEN is a beer". Note that you can use a Lucene syntax.
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/query-string-query.html
 	 * @see http://lucene.apache.org/core/3_6_1/queryparsersyntax.html
 	 * 
@@ -185,6 +198,8 @@ public class SearchTest extends StartNode {
 		Assert.assertNotNull(sr);
 		Assert.assertNotNull(sr.getHits());
 		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
+		
+		logger.info("We found {} beers", sr.getHits().totalHits());
 	}
 
 	/**
@@ -192,6 +207,7 @@ public class SearchTest extends StartNode {
 	 * with price between 5 and 10.
 	 * <br>We should have some results (or we are really unlucky!).
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/range-query.html
 	 * 
 	 */
@@ -208,6 +224,8 @@ public class SearchTest extends StartNode {
 		Assert.assertNotNull(sr);
 		Assert.assertNotNull(sr.getHits());
 		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
+
+		logger.info("We found {} beers", sr.getHits().totalHits());
 	}
 
 	/**
@@ -218,6 +236,7 @@ public class SearchTest extends StartNode {
 	 * Beer javabeans to check that Elasticsearch has really return
 	 * what we were looking for.
 	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/text-query.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/range-query.html
 	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/bool-query.html
@@ -242,10 +261,148 @@ public class SearchTest extends StartNode {
 		Assert.assertNotNull(sr.getHits());
 		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
 
+		logger.info("We found {} beers", sr.getHits().totalHits());
+
 		for (SearchHit hit : sr.getHits()) {
 			Beer beer = BeerHelper.toBeer(hit.getSourceAsString());
 			Assert.assertEquals("Heineken", beer.getBrand());
 			Assert.assertTrue(beer.getPrice()>5 && beer.getPrice()<10);
 		}
 	}
+	
+	/**
+	 * We want to build a complex Query based on brand name and price fields and we want to filter results
+	 * to have only 1L or more beers.
+	 * <br>We must find Heineken beers with price between 5 and 10 and size more than 1.
+	 * <br>We should have some results (or we are really unlucky!).
+	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
+	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/text-query.html
+	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/range-query.html
+	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/bool-query.html
+	 */
+	@Test
+	public void query_and_filter_Search() throws Exception {
+		QueryBuilder query = QueryBuilders
+				.boolQuery()
+					.must(
+						QueryBuilders.textQuery("brand", "HEINEKEN")
+					)
+					.must(
+						QueryBuilders.rangeQuery("price").from(5).to(10)
+					);
+		FilterBuilder filter = FilterBuilders.rangeFilter("size").from(1);
+		QueryBuilder qb = QueryBuilders.filteredQuery(query, filter);
+
+		logger.info("Your query is : {}", qb);
+
+		SearchResponse sr = node.client().prepareSearch().setQuery(qb)
+				.execute().actionGet();
+
+		Assert.assertNotNull(sr);
+		Assert.assertNotNull(sr.getHits());
+		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
+
+		logger.info("We found {} beers", sr.getHits().totalHits());
+		
+		for (SearchHit hit : sr.getHits()) {
+			Beer beer = BeerHelper.toBeer(hit.getSourceAsString());
+			Assert.assertEquals("Heineken", beer.getBrand());
+			Assert.assertTrue(beer.getPrice()>5 && beer.getPrice()<10);
+			Assert.assertTrue(beer.getSize()>1);
+		}
+
+		logger.info("Full json result is: {}", sr.toString());
+	}
+
+	/**
+	 * We want to search like google and see how scoring works on different documents.
+	 * <br>We will ask for the 100 first documents.
+	 * <br>We should have some results (or we are really unlucky!).
+	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
+	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/query-string-query.html
+	 * @see http://www.elasticsearch.org/guide/reference/api/search/from-size.html
+	 */
+	@Test
+	public void google_Search() throws Exception {
+		QueryBuilder qb = QueryBuilders.queryString("HEINEKEN pale");
+
+		logger.info("Your query is : {}", qb);
+
+		SearchResponse sr = node.client().prepareSearch().setQuery(qb)
+				.setSize(100)
+				.execute().actionGet();
+
+		Assert.assertNotNull(sr);
+		Assert.assertNotNull(sr.getHits());
+		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
+
+		logger.info("We found {} beers", sr.getHits().totalHits());
+		
+		int nbHeineken = 0;
+		int nbPale = 0;
+		for (SearchHit hit : sr.getHits()) {
+			Beer beer = BeerHelper.toBeer(hit.getSourceAsString());
+
+			if ("Heineken".equals(beer.getBrand())) nbHeineken++;
+			if (Colour.PALE.equals(beer.getColour())) nbPale++;
+		}
+		
+		logger.info("For the first 100 beers, we have {} Heineken and {} pale beers", nbHeineken, nbPale);
+		logger.info("Full json result is: {}", sr.toString());
+		
+
+	}
+
+	/**
+	 * We want to search like google and see how scoring works on different documents.
+	 * <br>We will boost the colour as colour is more important than brand.
+	 * <br>We will ask for the 100 first documents.
+	 * <br>We will highlight fields brand and colour
+	 * <br>We should have some results (or we are really unlucky!).
+	 * @throws Exception
+	 * @see http://www.elasticsearch.org/guide/reference/java-api/query-dsl.html
+	 * @see http://www.elasticsearch.org/guide/reference/query-dsl/query-string-query.html
+	 * @see http://www.elasticsearch.org/guide/reference/api/search/from-size.html
+	 * @see http://www.elasticsearch.org/guide/reference/api/search/highlighting.html
+	 */
+	@Test
+	public void google_with_boost_Search() throws Exception {
+		QueryBuilder qb = QueryBuilders.queryString("HEINEKEN pale^3");
+
+		logger.info("Your query is : {}", qb);
+
+		SearchResponse sr = node.client().prepareSearch().setQuery(qb)
+				.setSize(100)
+				.addHighlightedField("brand")
+				.addHighlightedField("colour")
+				.execute().actionGet();
+
+		Assert.assertNotNull(sr);
+		Assert.assertNotNull(sr.getHits());
+		Assert.assertTrue(sr.getHits().getTotalHits() > 0);
+
+		logger.info("We found {} beers", sr.getHits().totalHits());
+		
+		int nbHeineken = 0;
+		int nbPale = 0;
+		for (SearchHit hit : sr.getHits()) {
+			Beer beer = BeerHelper.toBeer(hit.getSourceAsString());
+
+			if ("Heineken".equals(beer.getBrand())) nbHeineken++;
+			if (Colour.PALE.equals(beer.getColour())) nbPale++;
+		}
+		
+		logger.info("For the first 100 beers, we have {} Heineken and {} pale beers", nbHeineken, nbPale);
+		
+		// We expect to have more or equals pale beers than heineken
+		Assert.assertTrue(nbPale >= nbHeineken);
+		
+		logger.info("Full json result is: {}", sr.toString());
+		
+
+	}
+
+	
 }
